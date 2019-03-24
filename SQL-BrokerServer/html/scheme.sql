481ba77c072263d9d74310129ee44f535d3ce470
ez29 <ez29@njit.edu>
2019-03-23 21:46:30 -0400

    Updated scheme

--- SQL-BrokerServer/html/scheme.sql
+++ SQL-BrokerServer/html/scheme.sql
@@ -16,6 +16,40 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `chats`
+--
+
+DROP TABLE IF EXISTS `chats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chats` (
+  `ChatId` int(11) NOT NULL AUTO_INCREMENT,
+  `ChatUsername` varchar(50) NOT NULL,
+  `ChatGameId` int(11) NOT NULL,
+  `ChatText` varchar(50) NOT NULL,
+  PRIMARY KEY (`ChatId`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `matches`
+--
+
+DROP TABLE IF EXISTS `matches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `matches` (
+  `Username` varchar(255) NOT NULL,
+  `Looking` int(11) DEFAULT NULL,
+  `turn` int(11) DEFAULT NULL,
+  `MatchId` int(11) DEFAULT NULL,
+  `currentTurn` int(11) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `playerHistory`
 --
 
@@ -24,32 +58,31 @@ DROP TABLE IF EXISTS `playerHistory`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `playerHistory` (
   `GameId` int(6) NOT NULL AUTO_INCREMENT,
-  `playerOneId` int(30) NOT NULL,
-  `playerTwoId` int(30) NOT NULL,
   `playerOneUser` varchar(30) NOT NULL,
   `playerTwoUser` varchar(30) NOT NULL,
-  `result` varchar(30) NOT NULL,
+  `winner` varchar(30) NOT NULL,
   `playerOneScore` int(30) NOT NULL,
   `playerTwoScore` int(30) NOT NULL,
-  `gameLength` time DEFAULT NULL,
+  `turnsUsed` time DEFAULT NULL,
   `gameDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`GameId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `testTable`
+-- Table structure for table `userTable`
 --
 
-DROP TABLE IF EXISTS `testTable`;
+DROP TABLE IF EXISTS `userTable`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `testTable` (
-  `Userid` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `userTable` (
+  `UserId` int(11) NOT NULL AUTO_INCREMENT,
   `Username` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL,
-  PRIMARY KEY (`Userid`)
+  PRIMARY KEY (`UserId`,`Username`),
+  UNIQUE KEY `Username` (`Username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -62,4 +95,4 @@ CREATE TABLE `testTable` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-08 19:15:44
+-- Dump completed on 2019-03-23 21:45:45
