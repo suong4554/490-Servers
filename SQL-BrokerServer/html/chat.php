6e4de5181409e25246ba23827a71c4d8df86648a
suong4554 <43896957+suong4554@users.noreply.github.com>
2019-03-25 22:55:46 -0400

    moved chat to sql/broker

---
+++ SQL-BrokerServer/html/chat.php
@@ -0,0 +1,74 @@
+<?php 
+class chat 
+{
+	private $ChatId,$ChatUsername,$ChatText,$ChatGameId;
+	
+	public function getChatId(){
+		return $this->ChatId;
+	}
+	public function setChatId($ChatId){
+		return $this->ChatId=$ChatId;
+	}	
+	
+	public function getChatUsername(){
+		return $this->ChatUsername;
+	}
+	public function setChatUsername($ChatUsername){
+		return $this->ChatUsername=$ChatUsername;
+	}	
+	
+	public function getChatText(){
+		return $this->ChatText;
+	}
+	public function setChatText($ChatText){
+		return $this->ChatText=$ChatText;
+	}	
+	public function getChatGameId(){
+		return $this->ChatGameId;
+	}
+	public function setChatGameId($ChatGameId){
+		return $this->ChatGameId=$ChatGameId;
+	}	
+	
+	public function InsertChatMessage()
+	{       
+		include "connectToDB.php";
+		$stmt = $db->prepare("INSERT INTO chats (ChatUsername,ChatGameId,ChatText) VALUES (?,?,?)");
+		$stmt->bind_param("sis",$ChatUsername,$ChatGameId,$ChatText);
+		$ChatUsername = $this->getChatUsername();
+		$ChatGameId = $this->getChatGameId();
+		$ChatText = $this->getChatText();
+		$stmt->execute();
+		$stmt->close();
+		$db->close();
+		
+		
+			
+	
+	}	
+	public function DisplayMessage()
+	{
+		include "connectToDB.php";
+		$ChatGameId = $this->getChatGameId();
+		$s = "select * from chats where ChatGameId = $ChatGameId ORDER BY ChatId DESC";
+		$t = mysqli_query($db,$s) or die (mysqli_error($db));
+		$numRows = mysqli_num_rows($t);
+		if($numRows > 0)
+		{
+			while($rowChat = mysqli_fetch_assoc($t))
+			{
+				if($rowChat["ChatGameId"] == $ChatGameId) 
+				{
+					$tempStr = "<span class='UserNameS'> $rowChat['ChatUsername'] </span> says: <span class='ChatMessageS'>$rowChat['ChatText']</span></br>";
+					return $tempStr;
+				}
+			}
+		}
+		if($numRows > 15)
+		{
+			$delete = "DELETE FROM chats ORDER BY ChatId LIMIT 1";
+			$statement = mysqli_query($db,$delete) or die (mysqli_error($db));
+		}
+	}
+}	
+?>
