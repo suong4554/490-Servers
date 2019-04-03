8010917f3f3383536c964117845b1c26b48c7c1e
suong4554 <43896957+suong4554@users.noreply.github.com>
2019-04-03 09:31:44 -0400

    local copy of bootstrap 3.3.1

--- ClientServer/html/index.php
+++ ClientServer/html/index.php
@@ -21,10 +21,9 @@ elseif($_SESSION["login"] == True){
 <head>
 
 <title>Login</title>
-<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script defer src="libraries/jquery-3.3.1.min.js"></script>
+<link rel="stylesheet" href="libraries/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+<script src="libraries/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 </head>
 <body>
 		<br>
