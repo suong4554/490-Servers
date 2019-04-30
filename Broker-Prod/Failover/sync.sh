66e99fd38c57e5099e1ada884347f599dd8f8e5f
Tortilla123 <jp568@njit.edu>
2019-04-30 12:38:21 -0400

    removing version-control from sync

--- Broker-Prod/Failover/sync.sh
+++ Broker-Prod/Failover/sync.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-sshpass -p "ubuntu" rsync -aP root@192.168.1.20:/home/v2/490-Servers/ /home/v2/490-Servers
+ sshpass -p "ubuntu" rsync -avz --exclude 'Version-Control/' root@192.168.1.20:/home/v2/490-Servers/ /home/v2/490-Servers
 
-
+ sshpass -p "ubuntu" rsync -avz --exclude 'Version-Control/' root@192.168.1.19:/home/v2/490-Servers/ /home/v2/490-Servers
 
