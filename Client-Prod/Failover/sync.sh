5b2a25479d930371f4d0708f1ad7030098f291c9
Tortilla123 <jp568@njit.edu>
2019-04-30 12:47:01 -0400

    removing version control from sync

--- Client-Prod/Failover/sync.sh
+++ Client-Prod/Failover/sync.sh
@@ -1,6 +1,8 @@
 #!bin/bash
 
-sshpass -p "ubuntu" rsync -aP root@192.168.1.12:/home/v2/490-Servers/ /home/v2/490-Servers
+ sshpass -p "ubuntu" rsync -avz --exclude 'Version-Control/' root@192.168.1.12:/home/v2/490-Servers/ /home/v2/490-Servers
+
+ sshpass -p "ubuntu" rsync -avz --exclude 'Version-Control/' root@192.168.1.13:/home/v2/490-Servers/ /home/v2/490-Servers
 
 
 
