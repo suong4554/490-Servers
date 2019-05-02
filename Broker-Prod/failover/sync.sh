d17b568f5de9f8f40e9dbe7947e75e99304c7989
Tortilla123 <jp568@njit.edu>
2019-05-01 23:28:22 -0400

    updating syncing

---
+++ Broker-Prod/failover/sync.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+echo "starting script" 
+#creates sql dump in html folder
+mysql -u sfu5 -pnjit -h localhost userAccounts < /var/www/html/dump.sql
+
+echo "sql dump created"
+
+#syncs html folder to failsafe
+sshpass -p "njit" ssh root@192.168.1.19 'rsync -avz root@192.168.1.20:/var/www/html /var/www/html'
+
+echo "html folder synced"
+
+#executes sql command to sync sql database on failsafe
+sshpass -p "njit" ssh root@192.168.1.19 'mysqldump -u sfu5 -pnjit userAccounts > /var/www/html/dump.sql'
+
+echo "sql dump transferred"
+
+#rewrites permissions 
+sshpass -p "njit" ssh root@192.168.1.19 'chown -R www-data:www-data /var/www/html'
+sshpass -p "njit" ssh root@192.168.1.19 'chmod -R 755 /var/www/html'
+
+echo "permissions rewritten"
+
+#restarts rabbit service on failsafe
+sshpass -p "njit" ssh root@192.168.1.19 'sudo service localRabbit stop'
+echo "stopped localRabbit"
+sshpass -p "njit" ssh root@192.168.1.19 'sudo service localRabbit start'
+echo "started localRabbit"
